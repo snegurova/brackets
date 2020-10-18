7cded749dc129e0d7179f97a00d0c24f6788dcde
snegurova <snegurova@gmail.com>
2020-10-18 13:49:21 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.reduce((res, it) => res.concat(it));
+    let stack = [];
+    for (let el of str) {
+        let bracketConfigIndex = bracketsConfig.indexOf(el);
+        if (bracketConfigIndex % 2 === 0 && el !== stack[stack.length - 1]) {
+            stack.push(bracketsConfig[bracketConfigIndex + 1]);
+        } else if (stack.pop() !== bracketsConfig[bracketConfigIndex]) {
+            return false;
+        }
+    }
+    return stack.length === 0;
 }
+
+
